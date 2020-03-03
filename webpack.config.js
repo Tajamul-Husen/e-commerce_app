eead463fa9e99424ea50537655ef4cbfce0eac3a
Tajamul-Husen <tajamulhusen58@gmail.com>
2020-03-03 22:10:23 +0530

    setup project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "app.bundle.js",
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    port: 5000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ]
+};
