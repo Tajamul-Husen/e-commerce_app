9fcdb3a5959ba6097f7f9106f2e3250822661013
Tajamul-Husen <tajamulhusen58@gmail.com>
2020-03-07 21:01:28 +0530

    second commit:navbar complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  return <div>Hello World!!</div>;
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const handleNav = e => {
+    if (e.target.classList.contains("overlay")) {
+      setToggleNav(false);
+    }
+    setToggleNav(!toggleNav);
+  };
+
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <h2 className="brand__title">Brand</h2>
+      </div>
+      <div
+        className={toggleNav ? "nav__wrapper overlay" : "nav__wrapper"}
+        onClick={handleNav}
+      >
+        <nav className="nav">
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              Product
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              About
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#">
+              Contact
+            </a>
+          </li>
+        </nav>
+      </div>
+      <div className="icons">
+        <div className="cart">
+          <FontAwesomeIcon
+            icon={faShoppingCart}
+            className="shopping-cart-icon"
+          />
+          <span className="badge">0</span>
+        </div>
+        <div
+          className={toggleNav ? "hamburger hamburger-trans" : "hamburger"}
+          onClick={handleNav}
+        >
+          <div className="hamburger__line"></div>
+          <div className="hamburger__line"></div>
+          <div className="hamburger__line"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
